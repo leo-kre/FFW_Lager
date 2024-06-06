1fb373b6aabbd69bc95b89b82028b17988591092
lfk <lf.kre@t-online.de>
2024-06-06 20:38:51 +0200

    fixing id input spacing ; updating layout + adding Header to qr code

--- src/app/qr_code/page.tsx
+++ src/app/qr_code/page.tsx
@@ -5,6 +5,7 @@ import { Html5Qrcode } from "html5-qrcode";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import cameraImage from "../../../public/camera.svg"; // Importiere das Bild korrekt
+import Header from "@/components/Header";
 
 export default function QR_Scanner() {
       const router = useRouter();
@@ -17,7 +18,7 @@ export default function QR_Scanner() {
       }
 
       const startScanner = () => {
-            const config = { fps: 10, qrbox: { width: 300, height: 300 }, rememberLastUsedCamera: true };
+            const config = { fps: 10, qrbox: { width: 350, height: 350 }, rememberLastUsedCamera: true };
             const qrCodeSuccessCallback = (decodedText: any) => {
                   setResult(decodedText);
             };
@@ -26,7 +27,8 @@ export default function QR_Scanner() {
             };
 
             const scanner = new Html5Qrcode("reader");
-            setHtml5QrCode(scanner);
+            let sc: any = scanner;
+            setHtml5QrCode(sc);
             scanner.start({ facingMode: "environment" }, config, qrCodeSuccessCallback, qrCodeErrorCallback).catch((error) => {
                   console.error("Unable to start the QR scanner. ", error);
             });
@@ -36,7 +38,8 @@ export default function QR_Scanner() {
       useEffect(() => {
             return () => {
                   if (html5QrCode) {
-                        html5QrCode.stop().catch((error) => {
+                        const h: any = html5QrCode;
+                        h.stop().catch((error: Error) => {
                               console.error("Failed to stop html5QrcodeScanner. ", error);
                         });
                   }
@@ -50,8 +53,10 @@ export default function QR_Scanner() {
       };
 
       return (
-            <main className="bg-slate-50 w-full min-h-screen flex flex-col justify-around items-center">
-                  <div id="container" className="w-[300px] aspect-square ring-2 rounded-default relative">
+            <main className="w-full min-h-screen flex flex-col items-center">
+                  <Header addItemButton={false} title="" closeButton={true}></Header>
+                  <div className="w-full h-full flex flex-col justify-around items-center mt-10"></div>
+                  <div id="container" className="w-[350px] aspect-square ring-2 rounded-default relative">
                         <div id="reader" className="absolute top-0 left-0 w-full h-full"></div>
                         {!scannerInitialized && (
                               <button onClick={handleManualScan} className=" text-black p-2 rounded absolute w-full h-full flex justify-center items-center">
