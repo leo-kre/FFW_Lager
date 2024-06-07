3e09bfbc48bab6aa1f3f68846d268ef51400c5d1
lfk <lf.kre@t-online.de>
2024-06-07 18:31:28 +0200

    undo scanner changes

--- src/app/qr_code/page.tsx
+++ src/app/qr_code/page.tsx
@@ -1,61 +1,87 @@
 "use client";
+
 import { useEffect, useState } from "react";
 import { Html5Qrcode } from "html5-qrcode";
-import { useRouter } from "next/navigation"; // Changed import for useRouter
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import cameraImage from "../../../public/camera.svg";
 import Header from "@/components/Header";
+
+const scannerSize = 300;
 
 export default function QR_Scanner() {
       const router = useRouter();
       const [result, setResult] = useState("No result");
       const [scannerInitialized, setScannerInitialized] = useState(false);
       const [html5QrCode, setHtml5QrCode] = useState(null);
+      const [error, setError] = useState(null);
 
-      useEffect(() => {
-            const startScanner = async () => {
-                  try {
-                        const config = { fps: 10, qrbox: { width: 350, height: 350 }, rememberLastUsedCamera: true };
-                        const qrCodeSuccessCallback = (decodedText) => {
-                              setResult(decodedText);
-                        };
-                        const qrCodeErrorCallback = (errorMessage) => {
-                              // Do nothing if no QR code is found
-                        };
+      if (result !== "No result") {
+            router.push("/item?id=" + result);
+      }
 
-                        const scanner = new Html5Qrcode("reader");
-                        setHtml5QrCode(scanner);
-                        await scanner.start({ facingMode: "environment" }, config, qrCodeSuccessCallback, qrCodeErrorCallback);
-                        setScannerInitialized(true);
-                  } catch (error) {
-                        console.error("Error starting scanner:", error);
-                  }
+      const startScanner = () => {
+            const config = {
+                  fps: 30,
+                  qrbox: { width: scannerSize, height: scannerSize },
+                  aspectRatio: 1.0,
+                  experimentalFeatures: {
+                        useBarCodeDetectorIfSupported: true,
+                  },
+                  videoConstraints: {
+                        facingMode: "environment",
+                        zoom: 4, // Apply a slight zoom
+                  },
             };
 
-            if (!scannerInitialized) {
-                  startScanner();
-            }
+            const qrCodeSuccessCallback = (decodedText: string) => {
+                  setResult(decodedText);
+            };
 
+            const qrCodeErrorCallback = (errorMessage: any) => {
+                  setError(errorMessage);
+            };
+
+            const scanner: any = new Html5Qrcode("reader");
+            setHtml5QrCode(scanner);
+            scanner
+                  .start({ facingMode: "environment" }, config, qrCodeSuccessCallback, qrCodeErrorCallback)
+                  .then(() => setScannerInitialized(true))
+                  .catch((error: Error) => {
+                        console.error("Unable to start the QR scanner. ", error);
+                  });
+      };
+
+      useEffect(() => {
             return () => {
+                  const htmlQR: any = html5QrCode;
                   if (html5QrCode) {
-                        html5QrCode.stop().catch((error) => {
+                        htmlQR.stop().catch((error: Error) => {
                               console.error("Failed to stop html5QrcodeScanner. ", error);
                         });
                   }
             };
-      }, [scannerInitialized, html5QrCode]);
+      }, [html5QrCode]);
+
+      const handleManualScan = () => {
+            if (!scannerInitialized) {
+                  setScannerInitialized(true);
+                  startScanner();
+            }
+      };
 
       return (
             <main className="w-full min-h-screen flex flex-col items-center">
                   <Header addItemButton={false} title="" closeButton={true}></Header>
-                  <div className="w-full h-full flex flex-col justify-around items-center mt-10"></div>
-                  <div id="container" className="w-[350px] min-h-[350px] h-fit ring-2 rounded-default relative">
-                        <div id="reader" className="absolute top-0 left-0 w-full h-full"></div>
-                        {!scannerInitialized && (
-                              <button className="hidden text-black p-2 rounded absolute w-full h-full flex justify-center items-center" id="scannerButton">
-                                    <Image src={cameraImage} alt="QR Code" width={100} height={100} />
-                              </button>
-                        )}
+                  <div className="w-full h-full flex flex-col justify-around items-center mt-10">
+                        <div id="container" className="w-[350px] min-h-[350px] h-fit ring-2 rounded-default relative">
+                              <div id="reader" className="absolute top-0 left-0 w-full h-full"></div>
+                              {!scannerInitialized && (
+                                    <button onClick={handleManualScan} className="text-black p-2 rounded absolute w-full h-full flex justify-center items-center">
+                                          <Image src={cameraImage} alt="QR Code" width={100} height={100} />
+                                    </button>
+                              )}
+                        </div>
                   </div>
             </main>
       );
