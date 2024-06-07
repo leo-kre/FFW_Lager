93e80b49f3e43df3e1889fb0b998528957b36043
lfk <lf.kre@t-online.de>
2024-06-07 17:41:01 +0200

    trying new scanner

--- src/app/qr_code/page.tsx
+++ src/app/qr_code/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { BrowserMultiFormatReader } from "@zxing/browser";
 import Image from "next/image";
 import cameraImage from "../../../public/camera.svg";
 import Header from "@/components/Header";
@@ -12,55 +11,33 @@ const scannerSize = 300;
 export default function QR_Scanner() {
       const router = useRouter();
       const [result, setResult] = useState("No result");
-      const [scannerInitialized, setScannerInitialized] = useState(false);
-      const [scanner, setScanner] = useState<BrowserMultiFormatReader | null>(null);
-      const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
 
-      useEffect(() => {
-            if (scannerInitialized && !scanner) {
-                  const codeReader = new BrowserMultiFormatReader();
-                  navigator.mediaDevices
-                        .getUserMedia({ video: { facingMode: "environment" } })
-                        .then((stream) => {
-                              setVideoStream(stream);
-                              const videoElement = document.getElementById("video");
-                              if (videoElement) {
-                                    videoElement.srcObject = stream;
-                                    videoElement.play();
-                              }
-                              return codeReader.decodeFromVideoElement(videoElement, (res, err) => {
-                                    if (res) {
-                                          setResult(res.getText());
-                                    }
-                              });
-                        })
-                        .catch((err) => console.error(err));
-                  setScanner(codeReader);
-            }
-      }, [scannerInitialized, scanner]);
-
-      useEffect(() => {
-            return () => {
-                  if (videoStream) {
-                        videoStream.getTracks().forEach((track) => track.stop());
-                  }
-                  if (scanner) {
-                        const videoElement = document.getElementById("video");
-                        if (videoElement) {
-                              videoElement.removeEventListener("play", scanner.decodeFromVideoElement);
+      const handleInputChange = async (e) => {
+            const file = e.target.files[0];
+            if (file) {
+                  try {
+                        const imageData = await decodeImage(file);
+                        const code = jsQR(imageData.data, imageData.width, imageData.height);
+                        if (code) {
+                              setResult(code.data);
+                              router.push("/item?id=" + code.data);
+                        } else {
+                              setResult("No QR code found");
                         }
+                  } catch (error) {
+                        console.error("Error decoding image: ", error);
+                        setResult("Error decoding image");
                   }
-            };
-      }, [videoStream, scanner]);
-
-      useEffect(() => {
-            if (result !== "No result") {
-                  router.push("/item?id=" + result);
             }
-      }, [result, router]);
+      };
 
       const handleManualScan = () => {
-            setScannerInitialized(true);
+            const input = document.createElement("input");
+            input.type = "file";
+            input.accept = "image/*";
+            input.capture = "camera";
+            input.onchange = handleInputChange;
+            input.click();
       };
 
       return (
@@ -68,13 +45,9 @@ export default function QR_Scanner() {
                   <Header addItemButton={false} title="" closeButton={true}></Header>
                   <div className="w-full h-full flex flex-col justify-around items-center mt-10"></div>
                   <div id="container" className="w-[250px] min-h-[250px] h-fit ring-2 rounded-default relative">
-                        {scannerInitialized ? (
-                              <video id="video" className="absolute top-0 left-0 w-full h-full rounded-default" />
-                        ) : (
-                              <button onClick={handleManualScan} className="text-black p-2 rounded absolute w-full h-full flex justify-center items-center">
-                                    <Image src={cameraImage} alt="QR Code" width={100} height={100} />
-                              </button>
-                        )}
+                        <button onClick={handleManualScan} className="text-black p-2 rounded absolute w-full h-full flex justify-center items-center">
+                              <Image src={cameraImage} alt="QR Code" width={100} height={100} />
+                        </button>
                   </div>
             </main>
       );
