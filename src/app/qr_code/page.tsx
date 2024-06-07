6fc97d80c180eee6b0802be4d6dd71ebc5b18dc4
lfk <lf.kre@t-online.de>
2024-06-07 17:27:38 +0200

    changing qr code software for better performance

--- src/app/qr_code/page.tsx
+++ src/app/qr_code/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Html5Qrcode } from "html5-qrcode";
 import { useRouter } from "next/navigation";
+import { BrowserMultiFormatReader } from "@zxing/browser";
 import Image from "next/image";
 import cameraImage from "../../../public/camera.svg";
 import Header from "@/components/Header";
@@ -13,58 +13,54 @@ export default function QR_Scanner() {
       const router = useRouter();
       const [result, setResult] = useState("No result");
       const [scannerInitialized, setScannerInitialized] = useState(false);
-      const [html5QrCode, setHtml5QrCode] = useState(null);
+      const [scanner, setScanner] = useState<BrowserMultiFormatReader | null>(null);
+      const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
 
-      if (result !== "No result") {
-            router.push("/item?id=" + result);
-      }
-
-      const startScanner = () => {
-            const config = {
-                  fps: 30, // Increased FPS for better scanning performance
-                  qrbox: { width: scannerSize, height: scannerSize }, // Adjusted QR box size
-                  rememberLastUsedCamera: true,
-                  aspectRatio: 1.0,
-                  experimentalFeatures: {
-                        useBarCodeDetectorIfSupported: true, // Use barcode detector if supported
-                  },
-                  videoConstraints: {
-                        facingMode: { exact: "environment" }, // Use back camera
-                        zoom: 2, // Apply a slight zoom
-                  },
-            };
-
-            const qrCodeSuccessCallback = (decodedText: string) => {
-                  setResult(decodedText);
-            };
-
-            const qrCodeErrorCallback = (errorMessage: any) => {
-                  // Do nothing if no QR code is found
-            };
-
-            const scanner: any = new Html5Qrcode("reader");
-            setHtml5QrCode(scanner);
-            scanner.start({ facingMode: "environment" }, config, qrCodeSuccessCallback, qrCodeErrorCallback).catch((error: Error) => {
-                  console.error("Unable to start the QR scanner. ", error);
-            });
-            setScannerInitialized(true);
-      };
+      useEffect(() => {
+            if (scannerInitialized && !scanner) {
+                  const codeReader = new BrowserMultiFormatReader();
+                  navigator.mediaDevices
+                        .getUserMedia({ video: { facingMode: "environment" } })
+                        .then((stream) => {
+                              setVideoStream(stream);
+                              const videoElement = document.getElementById("video");
+                              if (videoElement) {
+                                    videoElement.srcObject = stream;
+                                    videoElement.play();
+                              }
+                              return codeReader.decodeFromVideoElement(videoElement, (res, err) => {
+                                    if (res) {
+                                          setResult(res.getText());
+                                    }
+                              });
+                        })
+                        .catch((err) => console.error(err));
+                  setScanner(codeReader);
+            }
+      }, [scannerInitialized, scanner]);
 
       useEffect(() => {
             return () => {
-                  const htmlQR: any = html5QrCode;
-                  if (html5QrCode) {
-                        htmlQR.stop().catch((error: Error) => {
-                              console.error("Failed to stop html5QrcodeScanner. ", error);
-                        });
+                  if (videoStream) {
+                        videoStream.getTracks().forEach((track) => track.stop());
+                  }
+                  if (scanner) {
+                        const videoElement = document.getElementById("video");
+                        if (videoElement) {
+                              videoElement.removeEventListener("play", scanner.decodeFromVideoElement);
+                        }
                   }
             };
-      }, [html5QrCode]);
+      }, [videoStream, scanner]);
+
+      useEffect(() => {
+            if (result !== "No result") {
+                  router.push("/item?id=" + result);
+            }
+      }, [result, router]);
 
       const handleManualScan = () => {
-            if (!scannerInitialized) {
-                  startScanner();
-            }
+            setScannerInitialized(true);
       };
 
       return (
@@ -72,9 +68,10 @@ export default function QR_Scanner() {
                   <Header addItemButton={false} title="" closeButton={true}></Header>
                   <div className="w-full h-full flex flex-col justify-around items-center mt-10"></div>
                   <div id="container" className="w-[250px] min-h-[250px] h-fit ring-2 rounded-default relative">
-                        <div id="reader" className="absolute top-0 left-0 w-full h-full"></div>
-                        {!scannerInitialized && (
-                              <button onClick={handleManualScan} className=" text-black p-2 rounded absolute w-full h-full flex justify-center items-center">
+                        {scannerInitialized ? (
+                              <video id="video" className="absolute top-0 left-0 w-full h-full rounded-default" />
+                        ) : (
+                              <button onClick={handleManualScan} className="text-black p-2 rounded absolute w-full h-full flex justify-center items-center">
                                     <Image src={cameraImage} alt="QR Code" width={100} height={100} />
                               </button>
                         )}
