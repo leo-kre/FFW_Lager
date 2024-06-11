4012ce3a87362fea9299c989ce7799533b93c35d
lfk <lf.kre@t-online.de>
2024-06-11 10:35:29 +0200

    adding invalid code message

--- src/app/qr_code/page.tsx
+++ src/app/qr_code/page.tsx
@@ -8,19 +8,28 @@ import cameraImage from "../../../public/camera.svg";
 import Header from "@/components/Header";
 
 const scannerSize = 300;
+const errorMessageTimeoutInSeconds = 3;
 
 export default function QR_Scanner() {
       const router = useRouter();
       const [result, setResult] = useState("No result");
       const [scannerInitialized, setScannerInitialized] = useState(false);
       const [html5QrCode, setHtml5QrCode] = useState(null);
-      const [error, setError] = useState(null);
+      const [errorMessage, setErrorMessage] = useState("");
 
-      if (result !== "No result") {
-            router.push("/view_item?id=" + result);
-      }
-
-      let errorMessage: any = null;
+      useEffect(() => {
+            if (result !== "No result") {
+                  if (/^\d{4}$/.test(result)) {
+                        router.push("/view_item?id=" + result);
+                  } else {
+                        setErrorMessage("Ungültiger Code");
+                        setTimeout(() => {
+                              setErrorMessage("");
+                              setResult("No result");
+                        }, errorMessageTimeoutInSeconds * 1000);
+                  }
+            }
+      }, [result, router]);
 
       const startScanner = () => {
             const config = {
@@ -32,7 +41,7 @@ export default function QR_Scanner() {
                   },
                   videoConstraints: {
                         facingMode: "environment",
-                        zoom: 4, // Apply a slight zoom
+                        zoom: 4,
                   },
             };
 
@@ -40,14 +49,10 @@ export default function QR_Scanner() {
                   setResult(decodedText);
             };
 
-            const qrCodeErrorCallback = (errorMessage: any) => {
-                  setError(errorMessage);
-            };
-
             const scanner: any = new Html5Qrcode("reader");
             setHtml5QrCode(scanner);
             scanner
-                  .start({ facingMode: "environment" }, config, qrCodeSuccessCallback, qrCodeErrorCallback)
+                  .start({ facingMode: "environment" }, config, qrCodeSuccessCallback)
                   .then(() => setScannerInitialized(true))
                   .catch((error: Error) => {
                         console.error("Unable to start the QR scanner. ", error);
@@ -56,9 +61,9 @@ export default function QR_Scanner() {
 
       useEffect(() => {
             return () => {
-                  const htmlQR: any = html5QrCode;
-                  if (html5QrCode) {
-                        htmlQR.stop().catch((error: Error) => {
+                  const htmlCode: any = html5QrCode;
+                  if (htmlCode) {
+                        htmlCode.stop().catch((error: Error) => {
                               console.error("Failed to stop html5QrcodeScanner. ", error);
                         });
                   }
@@ -75,12 +80,12 @@ export default function QR_Scanner() {
             <main className="w-full min-h-screen flex flex-col items-center">
                   <Header addItemButton={false} title="" closeButton={true}></Header>
                   <div className="w-full h-full flex flex-col justify-around items-center mt-10">
-                        <h1 className="text-accent-red text-xl font-extrabold w-full text-center">{errorMessage}</h1>
+                        <h1 className="text-accent-red text-xl font-extrabold h-8 w-full text-center">{errorMessage}</h1>
                         <div id="container" className="aspect-square w-fit min-h-[300px] max-w-[90%] h-fit ring-2 rounded-default relative">
                               <div id="reader" className="top-0 left-0 w-full h-full rounded-default ring-2 rounded-default"></div>
 
                               {!scannerInitialized && (
-                                    <button onClick={handleManualScan} className=" text-black p-2 rounded absolute inset-0 flex justify-center items-center">
+                                    <button onClick={handleManualScan} className="text-black p-2 rounded absolute inset-0 flex justify-center items-center">
                                           <Image src={cameraImage} alt="QR Code" width={100} height={100} />
                                     </button>
                               )}
