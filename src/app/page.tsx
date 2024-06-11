363abfbde6862790dc608fd7af29f404b4f4adeb
lfk <lf.kre@t-online.de>
2024-06-11 10:20:54 +0200

    fixing home screen spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,16 @@ export default function Home() {
                         <h1 className="text-black">Suchen</h1>
                   </div>
 
-                  <div className="w-4/5 flex flex-col gap-4 mt-5">
-                        <a className="bg-accent-red w-full h-64 rounded-xl flex justify-center items-center" href="./qr_code">
+                  <div className="w-4/5 max-w-[80%] flex flex-col justify-center items-center gap-4 mt-5">
+                        <a className="bg-accent-red w-fit rounded-xl flex justify-center items-center" href="./qr_code">
                               <div className="w-fit h-fit flex justify-center items-center p-4">
-                                    <Image src={QR_CODE} alt="QR Code" width={200} height={200} />
+                                    <Image src={QR_CODE} alt="QR Code" width={175} height={175} />
                               </div>
                         </a>
 
-                        <a className="bg-accent-blue w-full h-64 rounded-xl flex justify-center items-center" href="./id">
+                        <a className="bg-accent-blue w-fit rounded-xl flex justify-center items-center" href="./id">
                               <div className="w-fit h-fit flex justify-center items-center p-4">
-                                    <Image src={PASSWORD} alt="PASSWORD" width={200} height={200} />
+                                    <Image src={PASSWORD} alt="PASSWORD" width={175} height={175} />
                               </div>
                         </a>
                   </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,39 @@
+"use client";
+
 import Image from "next/image";
-const closeButton = "../../public/cross.svg";
+import { useRouter } from "next/navigation";
+import closeButton from "../../public/close.svg";
+import addButton from "../../public/cross.svg";
 
 export default function Header(props: HeaderProps) {
+      const router = useRouter();
+
+      const navigateToHomeScreen = () => {
+            router.back();
+      };
+
       return (
-            <div className="w-full flex justify-between items-center my-5 mt-2 px-5">
-                  {props.closeButton ? <Image src={closeButton} alt={"Close Button"} width={50} height={50}></Image> : null}
-                  <h1 className="text-black font-extrabold text-4xl">{props.title}</h1>
-                  {props.closeButton ? <div className="w-5 h-5 bg-black"></div> : null}
+            <div className="w-full flex justify-between items-center md:my-5 my-2 mt-2 px-3">
+                  {props.closeButton ? (
+                        <div className="aspect-square w-8 flex">
+                              <Image
+                                    src={closeButton}
+                                    alt={"Close Button"}
+                                    width={20}
+                                    height={20}
+                                    onClick={() => {
+                                          navigateToHomeScreen();
+                                    }}
+                                    className="cursor-pointer "
+                              ></Image>
+                        </div>
+                  ) : null}
+                  <h1 className="text-black font-extrabold md:text-4xl text-2xl mt-1">{props.title}</h1>
+                  {props.closeButton ? (
+                        <div className="aspect-square w-8">
+                              <Image src={addButton} alt={"Add Button"} width={50} height={50} onClick={() => {}} className="cursor-pointer "></Image>
+                        </div>
+                  ) : null}
             </div>
       );
 }
