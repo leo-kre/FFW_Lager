580c5adc79054a831ec121d228915287b4d71184
Leon Kreutzer <lfk.kre@gmail.com>
2025-07-02 14:41:42 +0200

    adding next comment for easy sever setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
       env: {
+            //for server https://dev.leokre.de
             NEXT_PUBLIC_HOSTDOMAIN: "http://localhost:3000",
             NEXT_PUBLIC_LOCATION_LIST: "MTF - A0, MTF - A1.1, MTF - A1.2, MTF - A1.3, MTF - A2.1, MTF - A2.2, MTF - A2.3, MTF - A3.1, MTF - A3.2, MTF - A3.3, MTF - B0, MTF - B1.1, MTF - B1.2, MTF - B1.3, MTF - B2.1, MTF - B2.2, MTF - B2.3, MTF - B3.1, MTF - B3.2, MTF - B3.3, MTF - C0, MTF - C1.1, MTF - C1.2, MTF - C1.3, MTF - C2.1, MTF - C2.2, MTF - C2.3, MTF - C3.1, MTF - C3.2, MTF - C3.3, MTF - D0, MTF - D1.1, MTF - D1.2, MTF - D1.3, MTF - D2.1, MTF - D2.2, MTF - D2.3, MTF - D3.1, MTF - D3.2, MTF - D3.3, MTF - E0, MTF - E1.1, MTF - E1.2, MTF - E1.3, MTF - E2.1, MTF - E2.2, MTF - E2.3, MTF - E3.1, MTF - E3.2, MTF - E3.3",
 
